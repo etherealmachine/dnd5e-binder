3f3fdbd8781f627d527adde87b164b283cd626b4
James Pettit <etherealmachine@gmail.com>
2018-10-06 22:39:24 -0700

    Flexing stuff is hard.

--- src/App.tsx
+++ src/App.tsx
@@ -81,23 +81,35 @@ class App extends React.Component<Props, State> {
       <div className="column">
         {this.state.attacks.map((attack, i) => {
           return (<div key={`attack-${i}`} className="align-items-center">
-            <input name="attacks" data-index={i} data-key="name" value={this.state.attacks[i].name} onChange={this.state.handleInputChange} />
-            <input name="attacks" type="number" min="0" data-index={i} data-key="bonus" value={this.state.attacks[i].bonus} onChange={this.state.handleInputChange} />
-            <input name="attacks" data-index={i} data-key="damage_type"  value={this.state.attacks[i].damage_type} onChange={this.state.handleInputChange} />
+            <div>
+              <input name="attacks" data-index={i} data-key="name" value={this.state.attacks[i].name} onChange={this.state.handleInputChange} />
+            </div>
+            <div>
+              <input name="attacks" type="number" min="0" data-index={i} data-key="bonus" value={this.state.attacks[i].bonus} onChange={this.state.handleInputChange} />
+            </div>
+            <div>
+              <input name="attacks" data-index={i} data-key="damage_type"  value={this.state.attacks[i].damage_type} onChange={this.state.handleInputChange} />
+            </div>
             <button name="remove_attack" data-index={i} onClick={this.state.handleButtonClick}>-</button>
           </div>);
         })}
         <div className="align-items-center">
           <div className="column">
-            <input name="new_attack_name" type="text" value={this.state.new_attack_name} onChange={this.state.handleInputChange} />
+            <div>
+              <input name="new_attack_name" type="text" value={this.state.new_attack_name} onChange={this.state.handleInputChange} />
+            </div>
             <span className="align-self-center">Name</span>
           </div>
           <div className="column">
-            <input name="new_attack_bonus" type="number" min="0" value={this.state.new_attack_bonus} onChange={this.state.handleInputChange} />
+            <div>
+              <input name="new_attack_bonus" type="number" min="0" value={this.state.new_attack_bonus} onChange={this.state.handleInputChange} />
+            </div>
             <span className="align-self-center">Bonus</span>
           </div>
           <div className="column">
-            <input name="new_attack_damage_type" type="text" value={this.state.new_attack_damage_type} onChange={this.state.handleInputChange} />
+            <div>
+              <input name="new_attack_damage_type" type="text" value={this.state.new_attack_damage_type} onChange={this.state.handleInputChange} />
+            </div>
             <span className="align-self-center">Damage Type</span>
           </div>
           <div className="column">
@@ -113,25 +125,39 @@ class App extends React.Component<Props, State> {
           <div className="flex-3">
             <div className="column flex-1">
               <div>
-                <input className="flex-1" name="class" placeholder="Class" type="text" value={this.state.class} onChange={this.state.handleInputChange} />
-                <input name="level" placeholder="Level" type="number" min="1" value={this.state.level} onChange={this.state.handleInputChange} />
+                <div>
+                  <input name="class" placeholder="Class" type="text" value={this.state.class} onChange={this.state.handleInputChange} />
+                </div>
+                <div>
+                  <input name="level" placeholder="Level" type="number" min="1" value={this.state.level} onChange={this.state.handleInputChange} />
+                </div>
               </div>
-              <input name="race" placeholder="Race" type="text" value={this.state.race} onChange={this.state.handleInputChange} />
+              <div>
+                <input name="race" placeholder="Race" type="text" value={this.state.race} onChange={this.state.handleInputChange} />
+              </div>
             </div>
             <div className="column flex-1">
-              <input name="background" placeholder="Background" type="text" value={this.state.background} onChange={this.state.handleInputChange} />
-              <input name="alignment" placeholder="Alignment" type="text" value={this.state.alignment} onChange={this.state.handleInputChange} />
+              <div>
+                <input name="background" placeholder="Background" type="text" value={this.state.background} onChange={this.state.handleInputChange} />
+              </div>
+              <div>
+                <input name="alignment" placeholder="Alignment" type="text" value={this.state.alignment} onChange={this.state.handleInputChange} />
+              </div>
             </div>
             <div className="column flex-1">
-              <input name="player_name" placeholder="Player Name" type="text" value={this.state.player_name} onChange={this.state.handleInputChange} />
-              <input name="experience_points" placeholder="XP" type="number" min="0" value={this.state.experience_points} onChange={this.state.handleInputChange} />
+              <div>
+                <input name="player_name" placeholder="Player Name" type="text" value={this.state.player_name} onChange={this.state.handleInputChange} />
+              </div>
+              <div>
+                <input name="experience_points" placeholder="XP" type="number" min="0" value={this.state.experience_points} onChange={this.state.handleInputChange} />
+              </div>
             </div>
           </div>
         </div>
         <div>
-          <div className="main-column column flex-1">
+          <div className="main-column">
             <div className="column">
-              <div>
+              <div className="justify-content-space-around">
                 <div className="column">
                   {attributes}
                 </div>
@@ -148,12 +174,12 @@ class App extends React.Component<Props, State> {
                     {skills}
                     <h4>Skills</h4>
                   </div>
-                  <div className="column align-items-center">
-                    <input name="passive_perception" type="number" min="0" value={this.state.passive_perception} onChange={this.state.handleInputChange} />
-                    <h4><label htmlFor="passive_perception">Passive Perception (Wisdom)</label></h4>
-                  </div>
                 </div>
               </div>
+            </div>
+            <div className="column align-items-center">
+              <input name="passive_perception" type="number" min="0" value={this.state.passive_perception} onChange={this.state.handleInputChange} />
+              <h4><label htmlFor="passive_perception">Passive Perception (Wisdom)</label></h4>
             </div>
             <div className="column flex-1">
               <div className="column flex-1" style={{marginBottom: '20px'}}>
@@ -171,7 +197,7 @@ class App extends React.Component<Props, State> {
               <h4>Other Proficiencies &amp; Languages</h4>
             </div>
           </div>
-          <div className="main-column column flex-1">
+          <div className="main-column">
             <div className="column">
               <div className="justify-content-space-around">
                 <div className="column align-items-center">
@@ -190,28 +216,27 @@ class App extends React.Component<Props, State> {
                   <label htmlFor="speed">Speed</label>
                 </div>
               </div>
-              <div className="column">
+              <div className="justify-content-center justify-content-space-around">
                 <div className="column align-items-center">
-                  <input name="maximum_hit_points" type="number" min="0" max="1000" value={this.state.maximum_hit_points} onChange={this.state.handleInputChange} />
-                  <label htmlFor="maximum_hit_points">Maximum Hit Points</label>
+                  <span>&nbsp;</span>
+                  <input name="current_hit_points" type="number" min="0" max={this.state.maximum_hit_points} value={this.state.current_hit_points} onChange={this.state.handleInputChange} />
+                  <h4><label htmlFor="current_hit_points">HP</label></h4>
                 </div>
-                <div className="justify-content-space-around">
-                  <div className="column align-items-center">
-                    <span>&nbsp;</span>
-                    <input name="current_hit_points" type="number" min="0" max={this.state.maximum_hit_points} value={this.state.current_hit_points} onChange={this.state.handleInputChange} />
-                    <h4><label htmlFor="current_hit_points">Current HP</label></h4>
-                  </div>
-                  <div className="column align-items-center">
-                    <span>&nbsp;</span>
-                    <input name="temporary_hit_points" type="number" min="0" value={this.state.temporary_hit_points} onChange={this.state.handleInputChange} />
-                    <label htmlFor="temporary_hit_points">Temporary HP</label>
-                  </div>
-                  <div className="column align-items-center">
-                    <span>Total: {this.state.level}d{Rules.hit_dice_for_class[this.state.class]}</span>
-                    <input name="current_hit_dice" type="number" min="0" max={this.state.level} onChange={this.state.handleInputChange} />
-                    <label htmlFor="hit_dice">Hit Dice</label>
-                  </div>
-                 </div>
+                <div className="column align-items-center">
+                  <span>&nbsp;</span>
+                  <input name="maximum_hit_points" type="number" min="0" max="1000" value={this.state.maximum_hit_points} onChange={this.state.handleInputChange} />
+                  <label htmlFor="maximum_hit_points">Max HP</label>
+                </div>
+                <div className="column align-items-center">
+                  <span>Total: {this.state.level}d{Rules.hit_dice_for_class[this.state.class]}</span>
+                  <input name="current_hit_dice" type="number" min="0" max={this.state.level} onChange={this.state.handleInputChange} />
+                  <label htmlFor="hit_dice">Hit Dice</label>
+                </div>
+                <div className="column align-items-center">
+                  <span>&nbsp;</span>
+                  <input name="temporary_hit_points" type="number" min="0" value={this.state.temporary_hit_points} onChange={this.state.handleInputChange} />
+                  <label htmlFor="temporary_hit_points">Temp HP</label>
+                </div>
               </div>
             </div>
             <div className="column flex-1">
@@ -231,7 +256,7 @@ class App extends React.Component<Props, State> {
               </div>
             </div>
           </div>
-          <div className="main-column column flex-1">
+          <div className="main-column">
             <div className="column">
               <div className="column">
                 <textarea name="personality_traits" value={this.state.personality_traits} onChange={this.state.handleTextAreaChange} />
