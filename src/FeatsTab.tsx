3d4f9b6e8fd610d767cfe9a8f6fca27a5d39b013
James Pettit <james@academia.edu>
2018-11-02 16:07:06 -0700

    Move feats and races out of table.

--- src/FeatsTab.tsx
+++ src/FeatsTab.tsx
@@ -1,11 +1,9 @@
 import "core-js/library";
 import * as React from 'react';
-import { AutoSizer, Column, Table, TableCellProps, CellMeasurer, CellMeasurerCache, SortDirection, SortDirectionType } from 'react-virtualized';
 import { createStyles, WithStyles, withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 
-import { State as AppState, store } from './store';
-import Button from '@material-ui/core/Button';
+import { State as AppState } from './store';
 import TextField from '@material-ui/core/TextField';
 
 export interface Props extends WithStyles<typeof styles> {
@@ -14,8 +12,6 @@ export interface Props extends WithStyles<typeof styles> {
 
 interface State {
   query: string,
-  sortBy?: string,
-  sortDirection?: SortDirectionType,
 }
 
 const styles = createStyles({
@@ -25,27 +21,11 @@ const styles = createStyles({
     flex: '1',
   },
   table: {
-    flex: '1',
-    marginBottom: '20px',
-    overflow: 'hidden',
-  },
-  wrap: {
-    whiteSpace: 'normal',
-  },
-  row: {
-    boxSizing: 'border-box',
-    borderBottom: '1px solid #e0e0e0',
-  },
-  odd: {
-    boxSizing: 'border-box',
-    borderBottom: '1px solid #e0e0e0',
-    backgroundColor: '#fafafa',
-  },
-  text: {
-    padding: '10px 0',
     display: 'flex',
     flexDirection: 'column',
-  }
+    flex: '1',
+    marginBottom: '20px',
+  },
 });
 
 class FeatsTab extends React.Component<Props, State> {
@@ -54,8 +34,6 @@ class FeatsTab extends React.Component<Props, State> {
     super(props);
     this.state = {
       query: '',
-      sortBy: undefined,
-      sortDirection: undefined,
     };
   }
 
@@ -66,118 +44,38 @@ class FeatsTab extends React.Component<Props, State> {
   }
 
   private handleSearchChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // TODO: keep cache but use remapped index when filtering list.
-    this.cache.clearAll();
     this.setState({
       query: event.currentTarget.value,
     });
   }
 
-  private cache = new CellMeasurerCache({
-    fixedWidth: true,
-    minHeight: 25,
-  });
-
-  private renderText = ({cellData, dataKey, parent, rowIndex}: any): JSX.Element => {
-    let content = <span></span>;
-    if (cellData) {
-      const { classes } = this.props;
-      const text = cellData;
-      if (text instanceof Array) {
-        content = <div className={classes.text}>
-          {text.filter((text: string) => text).map((line, i) => <div key={i}>{line}</div>)}
-        </div>;
-      } else {
-        content = <span className={classes.text}>{text}</span>;
-      }
+  private renderFeat = (feat: any, index: number): JSX.Element => {
+    let text = feat.text;
+    if (!(text instanceof Array)) {
+      text = [text];
     }
-    return <CellMeasurer
-        cache={this.cache}
-        columnIndex={0}
-        key={dataKey}
-        parent={parent}
-        rowIndex={rowIndex}>
-      {content}
-    </CellMeasurer>;
-  }
-
-  private sort = ({sortBy, sortDirection}: {sortBy?: string, sortDirection?: SortDirectionType}) => {
-    const {
-      sortDirection: prevSortDirection
-    } = this.state;
-    this.cache.clearAll();
-
-    // If list was sorted DESC by this column.
-    // Rather than switch to ASC, return to "natural" order.
-    if (prevSortDirection === SortDirection.DESC) {
-      sortBy = undefined;
-      sortDirection = undefined;
-    }
-
-    this.setState({ sortBy, sortDirection });
-  }
-
-  private compare = (sortBy: string, sortDirection: SortDirectionType) => {
-    return (a: any, b: any) => {
-      const direction = sortDirection === SortDirection.ASC ? 1 : -1;
-      return a[sortBy].toString().toLowerCase() <= b[sortBy].toString().toLowerCase() ? -direction : direction;
-    };
+    return <div key={index}>
+      <h2>{feat.name}</h2>
+      <p><span className="term">Modifier: {feat.modifier}</span></p>
+      <p>
+        {(text as string[]).map((t, j) => <span key={j}>{t}<br /></span>)}
+      </p>
+    </div>;
   }
 
   public render() {
     const { classes, compendium } = this.props;
-    const { query, sortBy, sortDirection } = this.state;
+    const { query } = this.state;
     const list = Object.values(compendium).filter((obj) => query === '' || obj.name.toLowerCase().includes(query.toLowerCase()));
-    list.sort(this.compare(sortBy || 'name', sortDirection || SortDirection.ASC));
-    return <div style={{display: 'flex', flexDirection: 'column', flex: '1'}}>
+    return <div className={classes.container}>
       <TextField
           label="Search Feats"
           type="search"
           margin="normal"
           value={this.state.query}
           onChange={this.handleSearchChanged} />
-      <div style={{flex: '1', marginBottom: '20px'}}>
-        <AutoSizer>
-          {({height, width}) => (
-            <Table
-                headerHeight={30}
-                height={height}
-                rowClassName={({index}: {index: number}) => (index % 2 == 0 ? classes.row : classes.odd)}
-                noRowsRenderer={() => <div>No rows</div>}
-                rowHeight={this.cache.rowHeight}
-                rowGetter={({index}: {index: number}) => list[index]}
-                rowCount={list.length}
-                width={width}
-                sort={this.sort}
-                sortBy={sortBy}
-                sortDirection={sortDirection}>
-              <Column
-                label="name"
-                cellDataGetter={({rowData}) => rowData.name}
-                dataKey="name"
-                width={250} />
-              <Column
-                label="prerequisite"
-                dataKey="prerequisite"
-                flexGrow={1}
-                width={0} />
-              <Column
-                disableSort
-                label="text"
-                dataKey="text"
-                cellRenderer={this.renderText}
-                className={classes.wrap}
-                flexGrow={4}
-                width={0} />
-              <Column
-                disableSort
-                label=""
-                dataKey="name"
-                cellRenderer={(col: TableCellProps) => <Button onClick={() => store.dispatch({type: 'ADD_TO_CHARACTER', feat: col.cellData})}>+</Button>}
-                width={50} />
-            </Table>
-          )}
-        </AutoSizer>
+      <div className={classes.table}>
+        {list.map(this.renderFeat)}
       </div>
     </div>;
   }
--- src/RacesTab.tsx
+++ src/RacesTab.tsx
@@ -1,11 +1,9 @@
 import "core-js/library";
 import * as React from 'react';
-import { AutoSizer, Column, Table, TableCellProps, CellMeasurer, CellMeasurerCache, SortDirection, SortDirectionType } from 'react-virtualized';
 import { createStyles, WithStyles, withStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 
-import { State as AppState, store } from './store';
-import Button from '@material-ui/core/Button';
+import { State as AppState } from './store';
 import TextField from '@material-ui/core/TextField';
 
 export interface Props extends WithStyles<typeof styles> {
@@ -14,8 +12,6 @@ export interface Props extends WithStyles<typeof styles> {
 
 interface State {
   query: string,
-  sortBy?: string,
-  sortDirection?: SortDirectionType,
 }
 
 const styles = createStyles({
@@ -25,29 +21,10 @@ const styles = createStyles({
     flex: '1',
   },
   table: {
-    flex: '1',
-    marginBottom: '20px',
-    overflow: 'hidden',
-  },
-  wrap: {
-    whiteSpace: 'normal',
-  },
-  row: {
-    boxSizing: 'border-box',
-    borderBottom: '1px solid #e0e0e0',
-  },
-  odd: {
-    boxSizing: 'border-box',
-    borderBottom: '1px solid #e0e0e0',
-    backgroundColor: '#fafafa',
-  },
-  trait: {
-    padding: '10px 0',
     display: 'flex',
     flexDirection: 'column',
-  },
-  traitName: {
-    fontWeight: 600,
+    flex: '1',
+    marginBottom: '20px',
   },
 });
 
@@ -57,8 +34,6 @@ class RacesTab extends React.Component<Props, State> {
     super(props);
     this.state = {
       query: '',
-      sortBy: undefined,
-      sortDirection: undefined,
     };
   }
 
@@ -69,133 +44,47 @@ class RacesTab extends React.Component<Props, State> {
   }
 
   private handleSearchChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-    // TODO: keep cache but use remapped index when filtering list.
-    this.cache.clearAll();
     this.setState({
       query: event.currentTarget.value,
     });
   }
 
-  private cache = new CellMeasurerCache({
-    fixedWidth: true,
-    minHeight: 25,
-  });
-
-  private renderTraits = ({cellData, dataKey, parent, rowIndex}: any): JSX.Element => {
-    let content = <span></span>;
-    if (cellData) {
-      const { classes } = this.props;
-      const traits = cellData;
-      if (traits instanceof Array) {
-        content = <div className={classes.trait}>
-          {traits.map((trait, i) => <div key={i}><span className={classes.traitName}>{trait.name}:</span> {trait.text}</div>)}
-        </div>;
-      } else {
-        content = <span className={classes.trait}><span className={classes.traitName}>{traits.name}:</span> {traits.text}</span>;
+  private renderRace = (race: any, index: number): JSX.Element => {
+    const content: JSX.Element[] = [];
+    let traits = race.trait;
+    if (!(traits instanceof Array)) {
+      traits = [traits];
+    }
+    traits.forEach((trait: any) => {
+      let text = trait.text;
+      if (!(trait.text instanceof Array)) {
+        text = [trait.text];
       }
-    }
-    return <CellMeasurer
-        cache={this.cache}
-        columnIndex={0}
-        key={dataKey}
-        parent={parent}
-        rowIndex={rowIndex}>
+      content.push(
+        <p key={content.length}><span className="term">{trait.name}:</span>&nbsp;{(text as string[]).map((t, j) => <span key={j}>{t}<br /></span>)}</p>)
+    });
+    return <div key={index}>
+      <h2>{race.name}</h2>
+      <p><span className="term">Ability: {race.ability}</span></p>
+      <p><span className="term">Size: {race.size}</span></p>
+      <p><span className="term">Speed: {race.speed}</span></p>
       {content}
-    </CellMeasurer>;
-  }
-
-  private sort = ({sortBy, sortDirection}: {sortBy?: string, sortDirection?: SortDirectionType}) => {
-    const {
-      sortDirection: prevSortDirection
-    } = this.state;
-    this.cache.clearAll();
-
-    // If list was sorted DESC by this column.
-    // Rather than switch to ASC, return to "natural" order.
-    if (prevSortDirection === SortDirection.DESC) {
-      sortBy = undefined;
-      sortDirection = undefined;
-    }
-
-    this.setState({ sortBy, sortDirection });
-  }
-
-  private compare = (sortBy: string, sortDirection: SortDirectionType) => {
-    return (a: any, b: any) => {
-      const direction = sortDirection === SortDirection.ASC ? 1 : -1;
-      return a[sortBy].toString().toLowerCase() <= b[sortBy].toString().toLowerCase() ? -direction : direction;
-    };
+    </div>;
   }
 
   public render() {
     const { classes, compendium } = this.props;
-    const { query, sortBy, sortDirection } = this.state;
+    const { query } = this.state;
     const list = Object.values(compendium).filter((obj) => query === '' || obj.name.toLowerCase().includes(query.toLowerCase()));
-    list.sort(this.compare(sortBy || 'name', sortDirection || SortDirection.ASC));
-    return <div style={{display: 'flex', flexDirection: 'column', flex: '1'}}>
+    return <div className={classes.container}>
       <TextField
           label="Search Races"
           type="search"
           margin="normal"
           value={this.state.query}
           onChange={this.handleSearchChanged} />
-      <div style={{flex: '1', marginBottom: '20px'}}>
-        <AutoSizer>
-          {({height, width}) => (
-            <Table
-                headerHeight={30}
-                height={height}
-                rowClassName={({index}: {index: number}) => (index % 2 == 0 ? classes.row : classes.odd)}
-                noRowsRenderer={() => <div>No rows</div>}
-                rowHeight={this.cache.rowHeight}
-                rowGetter={({index}: {index: number}) => list[index]}
-                rowCount={list.length}
-                width={width}
-                sort={this.sort}
-                sortBy={sortBy}
-                sortDirection={sortDirection}>
-              <Column
-                label="name"
-                cellDataGetter={({rowData}) => rowData.name}
-                dataKey="name"
-                width={250} />
-              <Column
-                label="size"
-                dataKey="size"
-                width={60} />
-              <Column
-                label="speed"
-                dataKey="speed"
-                width={80} />
-              <Column
-                label="ability"
-                dataKey="ability"
-                className={classes.wrap}
-                flexGrow={1}
-                width={0} />
-              <Column
-                label="proficiency"
-                dataKey="proficiency"
-                className={classes.wrap}
-                flexGrow={1}
-                width={1} />
-              <Column
-                disableSort
-                label="traits"
-                dataKey="trait"
-                cellRenderer={this.renderTraits}
-                className={classes.wrap}
-                flexGrow={4}
-                width={0} />
-              <Column
-                disableSort
-                label=""
-                dataKey="name"
-                cellRenderer={(col: TableCellProps) => <Button onClick={() => store.dispatch({type: 'ADD_TO_CHARACTER', race: col.cellData})}>+</Button>}
-                width={50} />
-            </Table>
-          )}
-        </AutoSizer>
+      <div className={classes.table}>
+        {list.map(this.renderRace)}
       </div>
     </div>;
   }
