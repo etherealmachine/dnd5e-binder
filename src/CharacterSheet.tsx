545b939814caa8fd97bfd259af733574eaa414ef
James Pettit <etherealmachine@gmail.com>
2019-02-16 18:53:45 -0800

    Playing around with the stepper.

--- src/CharacterSheet.tsx
+++ src/CharacterSheet.tsx
@@ -3,26 +3,34 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 
+import Button from '@material-ui/core/Button';
 import Compendium from './compendium';
+import { CharacterState } from './store';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import { CharacterState } from './store';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
 import { State as AppState } from './store';
-import Typography from '@material-ui/core/Typography';
 
 export interface Props extends CharacterState {
   compendium: Compendium
   dispatch: Dispatch
 }
 
-interface State {
+interface State extends Partial<CharacterState> {
+  activeStep: number
 }
 
 class CharacterSheet extends React.Component<Props, State> {
 
   public constructor(props: Props) {
     super(props);
-    this.state = {};
+    const state = {
+      activeStep: 0
+    };
+    Object.assign(state, props);
+    this.state = state;
   }
 
   public static mapStateToProps(state: AppState): Partial<Props> {
@@ -37,43 +45,53 @@ class CharacterSheet extends React.Component<Props, State> {
     };
   }
 
+  private handleBack = () => {
+    this.setState({
+      activeStep: this.state.activeStep-1,
+    });
+  }
+
+  private handleNext = () => {
+    this.setState({
+      activeStep: this.state.activeStep+1,
+    });
+  }
+
   public render() {
     return <div>
+      <Stepper activeStep={this.state.activeStep}>
+        <Step>
+          <StepLabel>Race</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Class</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Name</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Background</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Ability Scores</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Proficiencies</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Equipment</StepLabel>
+        </Step>
+        <Step>
+          <StepLabel>Spells</StepLabel>
+        </Step>
+      </Stepper>
       <Card>
-        <CardContent>
-          <Typography variant="h5">Choose Class</Typography>
-        </CardContent>
+        <CardContent></CardContent>
       </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="h5">Choose Race</Typography>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="h5">Choose Background</Typography>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="h5">Assign Ability Scores</Typography>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="h5">Choose Proficiencies</Typography>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="h5">Buy Equipment</Typography>
-        </CardContent>
-      </Card>
-      <Card>
-        <CardContent>
-          <Typography variant="h5">Select Spells</Typography>
-        </CardContent>
-      </Card>
+      <div>
+        <Button disabled={this.state.activeStep === 0} onClick={this.handleBack}>Back</Button>
+        <Button color="primary" onClick={this.handleNext}>Next</Button>
+      </div>
     </div>;
   }
 }
